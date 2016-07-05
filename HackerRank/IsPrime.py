3557c7c67327aaa4bc4ccd6451bdf960643fbb55
Madhu Chakravarthy <madhu.chakravarthy@odc-ruckuswireless.com>
2016-07-05 19:29:17 +0530

    Adding HackerRank problem solutions

---
+++ HackerRank/IsPrime.py
@@ -0,0 +1,20 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+def isPrime(n):
+    if n <= 1:
+        return "Not prime"
+    elif n <= 3:
+        return "Prime"
+    elif (n % 2 == 0) or (n % 3 == 0):
+        return "Not prime"
+    i = 5
+    while i*i <= n:
+        if (n % i == 0) or (n % (i + 2) == 0):
+            return "Not prime"
+        i = i + 6
+    return "Prime"
+
+t = int(raw_input().strip())
+for i in range(t):
+    n = int(raw_input().strip())
+    print isPrime(n)
