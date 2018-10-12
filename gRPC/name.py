07192c8744ef4873a5c089b96a731ff942812c60
Madhu Chakravarthy <ncmadhu@gmail.com>
2018-10-12 11:33:41 -0700

    Renamed the files

--- gRPC/name.py
+++
@@ -1,9 +0,0 @@
-import first_pb2
-name = first_pb2.Name()
-name.firstname = "Madhu"
-name.lastname = "Chakravarthy"
-data = name.SerializeToString()
-person = first_pb2.Name()
-person.ParseFromString(data)
-print("First Name: %s" % person.firstname)
-print("Last Name: %s" % person.lastname)
